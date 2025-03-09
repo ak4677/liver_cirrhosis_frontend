121cbd23b1201bde0a6d0f90c917e5d48bd93f70
ak4677 <ak4677@srmist.edu.in>
2025-03-10 02:18:10 +0530

    recoverign files

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,26 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Login from './components/Login'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Docdes from './components/docdes'
 import Patides from './components/Patides'
-
+import Labentry from './components/Labentry'
+import DataFetch from './components/DataFetch'
+import Docdes from './components/Docdes'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter >
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Docdes" element={<Docdes/>} />
-        <Route path="/Patides" element={<Patides/>} />
-      </Routes>
-    </BrowserRouter>
-
+    <DataFetch>
+      <BrowserRouter >
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Docdes" element={<Docdes />} />
+          <Route path="/Patides" element={<Patides />} />
+          <Route path="/Labentry" element={<Labentry />} />
+        </Routes>
+      </BrowserRouter>
+    </DataFetch>
   )
 }
 
